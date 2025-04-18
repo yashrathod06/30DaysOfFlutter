01e2a7c6f63756c3a956b8f0b5742562b86e11d7
yashrathod06 <yashrathod0708@gmail.com>
2025-04-18 22:41:02 +0530

    Home Page Create.

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    final String name = "Yash";
+    return Scaffold(
+      appBar: AppBar(title: Text("Catalog Spp")),
+      body: Center(
+        child: Container(child: Text("Welcome To $days days flutter $name")),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(child: Text("Welcome To 30 days flutter.")),
-        ),
-      ),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
